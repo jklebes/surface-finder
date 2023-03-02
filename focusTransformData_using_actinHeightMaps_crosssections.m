5685e7457344525c08ff640cef30f54303512e15
jklebes <jklebes001@dundee.ac.uk>
2023-03-02 11:34:04 +0000

    before merge with transform

--- focusTransformData_using_actinHeightMaps_crosssections.m
+++ focusTransformData_using_actinHeightMaps_crosssections.m
@@ -1,4 +1,4 @@
-function focusTransformData_using_actinHeightMaps(file_folder_actin, filefolder_myosinORnuclei, out_folder_actin, outfolder_myosinORnuclei, time_points, workers, offsets, varargin)
+function focusTransformData_using_actinHeightMaps_crosssections(file_folder_actin, filefolder_myosinORnuclei, out_folder_actin, outfolder_myosinORnuclei, xsection_out_folder_actin, xsection_outfolder_myosinORnuclei, time_points, workers, offsets, varargin)
 %{
 %     Function to focus the surface of the embryo using 45 degrees transformed data. 
 %     This is a modified verision of the square gradient focusing algorithm. 
@@ -34,7 +34,7 @@ function focusTransformData_using_actinHeightMaps(file_folder_actin, filefolder_
 %     Guillermo 2021
 
 %}
-
+import utilities.expReader;
 
 %% Parse arguments
 %TODO parse arguments propoerly
@@ -51,15 +51,19 @@ elseif pool.NumWorkers~=workers
     delete(pool);
     parpool(workers);
 end
-
-for t_ind=1:length(time_points) %parfor
-
+%all_surfaces=cell(length(time_points),1); %collect in cell array by
+%index
+parfor t_ind=1:length(time_points) %parfor
+    surfaces_=cell(2);
+    if ismember(t_ind, time_points)
+          
     %% Read in raw input file - actin
     info_filename=[file_folder_actin filesep '_info.txt'];
     rawInputImage=[file_folder_actin filesep 'image_' num2str(t_ind,'%.04d') '_trans.raw'];
     disp(['image ' num2str(t_ind) '  start read']);
     tstart = tic;
     img_vol_actin = read_3D_image(rawInputImage, info_filename);
+    
     tend = toc(tstart);
     disp(['tmp_trans_ actin data reading ' num2str(t_ind) ' took ' num2str(tend) ' seconds.'])
 
@@ -69,24 +73,46 @@ for t_ind=1:length(time_points) %parfor
 
     %% save heightmap
     [~,~, Depth] = size(img_vol_actin);
-    save_heightmap(heightmap, Depth, out_folder_actin)
+    save_heightmap(heightmap, Depth, t_ind, out_folder_actin)
 
     %% extract and save actin surfaces
-    select_surfaces(img_vol_actin, t_ind, heightmap, offsets);
-
-    %% read myosin data
+    surfaces_{1}=select_surfaces(img_vol_actin, t_ind, heightmap, offsets, out_folder_actin);
+    
+    %% read second channel data
     info_filename=[filefolder_myosinORnuclei filesep '_info.txt'];
     rawInputImage=[filefolder_myosinORnuclei filesep 'image_' num2str(t_ind,'%.04d') '_trans.raw'];
     img_vol_myosin = read_3D_image(rawInputImage, info_filename);
 
-    %% extract and save myosin surfaces, using heightmap from actin
-    select_surfaces(img_vol_myosin,  t_ind, heightmap, offsets);
+    heightmap_myosin=calculate_heightmap(img_vol_myosin, overlaps, sq_side, kernel_size); %from actin channel
 
-    %% do cross sections, while the 3D data is in memory
-    cross_sections(img_vol_actin);
-    cross_sections(img_vol_myosin);
-
+    %% extract and save myosin surfaces
+    surfaces_{2}=select_surfaces(img_vol_myosin ,  t_ind, heightmap_myosin, offsets, outfolder_myosinORnuclei);
+    %fill array to return
+    %all_surfaces{t_ind}=surfaces_;
+    else 
+    disp(["time point " num2str(t_ind) " not found"]);
+    %placeholder for 3D data in cell array remains []
+    end
+    %% do cross sections, while the 3D data for tindex-1, tindex is in memory
+    cross_sections(img_vol_actin, xsection_out_folder_actin, t_ind-1);
+    cross_sections(img_vol_actin, xsection_outfolder_myosinORnuclei, t_ind-1);
 end % parfor
+%(so that parofr can slice by index)
+%now we have all_surfaces, a cell array of t/2 time point pairs, each
+%containing a 2x2 cell array in actin and myosin, pair upper and lower halves,
+%each containing a Height x Widths x n_offsets array of images
+
+%reshape all_surfaces before returning
+%into a cell array of timepoints x channels
+% all_surfaces_t = cell(length(time_points),1);
+% first=@(x) x(1,:);
+% all_surfaces_t(1:2:end,1) = cellfun(first,all_surfaces,'UniformOutput',false);
+% second=@(x) x(2,:);
+% all_surfaces_t(2:2:end,1) = cellfun(second,all_surfaces,'UniformOutput',false);
+% vertcat(all_surfaces_t{:})
+% %each entry all_surfaces_t(t_ind, channel) contains
+% % a Height x Width x n_offsets stack of images
+% 
 end %function
 
 function data=read_3D_image(filename, info_filename)
@@ -169,21 +195,24 @@ kernel = ones(kernel_size)./kernel_size^2;
 heightMap=round(imfilter(heightMap,kernel,'same'));
 end
 
-function save_heightmap(heightMap, Depth, out_folder_actin)
+function save_heightmap(heightMap, Depth, t_ind, out_folder_actin)
         h_map_folder = [out_folder_actin filesep 'h_map'];
         mkdir(h_map_folder);
         heightMap_image = uint8(heightMap*(256/Depth)); % transform heightmap to 8 bit
-        imwrite(heightMap_image, [h_map_folder filesep 'img_' num2str(t_ind,'%.04d') '.tif']);
+        imwrite(heightMap_image, [h_map_folder filesep 'img_' num2str(t_ind,'%.04d') '.jpeg']);
 end
 
-function select_surfaces(img_vol, t_ind, heightMap, offsets)
+function surfaces=select_surfaces(img_vol, t_ind, heightMap, offsets, out_folder)
+[Height, Width, Depth]=size(img_vol);
+surfaces=zeros(Height, Width, size(offsets,2));
+offset_index=1;
 for offset = offsets
-    final_folder = [out_folder_actin filesep 'surface_' num2str(offset,'%02d')];
+    final_folder = [out_folder filesep 'surface_' num2str(offset,'%02d')];
     mkdir(final_folder);
 
     % produce the surface. The image is the average of 3 planes in the surface
     z_averaging = -1:1;%-1:1; %-5:5
-    vol = zeros(size(heightMap,1), size(heightMap,2), length(z_averaging), 'uint8');
+    vol = zeros(Height, Width, length(z_averaging), 'uint8');
     counter = 1;
     %TODO array operations?
     for z=z_averaging
@@ -199,7 +228,44 @@ for offset = offsets
     %             figure;
     %             imshow(surface_00);
 
-    imwrite(surface,  [final_folder filesep 'img_' num2str(t_ind,'%.04d') '.tif']);
+    imwrite(surface,  [final_folder filesep 'img_' num2str(t_ind,'%.04d') '.jpeg']);
+    surfaces(:,:,offset_index) = surface;
+    offset_index=offset_index+1;
+end
 
 end
+
+function cross_sections(img_vol, out_folder, t_ind)
+opt_arguments = {
+    1,... % double_scan
+    [1;2],... % order of scans. Left:2, Right:1
+    5,... % number of scans
+    };
+
+%double_scan = opt_arguments{1};
+scan_order = opt_arguments{2};
+num_scan = opt_arguments{3};
+
+%detect whether double scan by input format: call array of volumes or
+%volume
+
+
+[Height, Width, Depth]=size(img_vol);
+
+scan_positions = round(linspace(1,Width,num_scan));
+
+xsection = uint8(zeros(num_scan*Depth, Height));
+
+scan_counter = 0;
+for xs = scan_positions
+    origin = scan_counter*Depth+1;
+    xsection(origin:origin+Depth-1,1:Height) = squeeze(img_vol(:,xs,:))';
+    scan_counter = scan_counter + 1;
+end
+
+
+name_idx = t_ind;
+
+mkdir(out_folder)
+imwrite(xsection, [out_folder filesep 'img_', num2str(name_idx, '%04d') '.jpeg'])
 end
